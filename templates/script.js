ea480dde956bbf2dba8fecf8fd319a35fd1ac1b0
Jeff Trojan <jefdauda@gmail.com>
2022-09-13 00:27:16 +0200

    Fixed bug serving template

---
+++ templates/script.js
@@ -0,0 +1,16 @@
+function generatecode(value) {
+    let formdata = new formdata();
+    formdata.append('data', value);
+    fetch('http://localhost:5000/generate', {
+    method: 'POST',
+    body: formdata
+    }).then((res) => {
+        res.body.getReader().read().then((img)=> {
+            let img_array = img.value;
+            let stringvalue = String.fromCharCode(...img_array);
+            let encodedvalue = btoa(stringvalue);
+            document.getElementById('qrcode').src = `data:image/png;base64,${encodedvalue}`;
+
+        })
+    })
+}
